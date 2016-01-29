aaca798c6f8a4f1b01e9ff7da41836718b5de2cc
Mathew Glodack <glodack@gmail.com>
2016-01-28 21:04:29 -0800

    Webpack this business
    * Update index js to be jsx style
    * Update webpack config

--- index.js
+++ index.js
@@ -4,14 +4,12 @@ var React = require('react');
 var HelloWorld = React.createClass({
   render: function() {
     return (
-      React.createElement('div', null, "Say something different")
+      <div>Hello World</div>
     );
   }
  });
 
 ReactDOM.render(
-  React.createElement(HelloWorld, null),
+  <HelloWorld />,
   document.getElementById('content')
 );
-
-
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  entry: "./index.js",
+  output: {
+    path: "dist",
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  }
+};
